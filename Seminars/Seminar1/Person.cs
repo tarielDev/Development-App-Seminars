08b58f3b0d55f2e97b6b8eaed52da304fdd3d675
Takhir Galimzianov <tariel@post.com>
2023-10-25 19:49:33 +0400

    Добавил методы в класс Person

--- Seminars/Seminar1/Person.cs
+++ Seminars/Seminar1/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,7 @@ namespace Seminar1
         public List<Person> Children { get; private set; }
         public List<Person> Siblings { get; private set; }
 
-        public Person(string name, string surname, DateTime birthday, Gender sex)
+        public Person(string name, DateTime birthday, Gender sex)
         {
             this.Name = name;
             this.Birthday = birthday;
@@ -32,7 +33,60 @@ namespace Seminar1
             Children = new List<Person>();
         }
 
+        public string GetBirthday()
+        {
+            return Birthday.ToString("dd.MM.yyyy");
+        }
 
+        public void SetBirthday(string birth)
+        {
+            if (DateTime.TryParseExact(birth, "dd.MM.yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out var parsedBirth))
+            {
+                Birthday = parsedBirth;
+            }
+            else
+            {
+                throw new ArgumentException("Введите дату рождения по формату 'дд.мм.гггг'.");
+            }
+        }
+
+        public void AddChild(Person child)
+        {
+            Children.Add(child);
+            child.Mother = this;
+            if (Sex == Gender.Male)
+                child.Father = this;
+        }
+
+        public void AddSibling(Person sibling)
+        {
+            Siblings.Add(sibling);
+            sibling.Siblings.Add(this);
+        }
+
+        public void PrintFamilyTree(int level, string relationship = null)
+        {
+
+            string prefix = new string('|', level) + new string('-', 3);
+            string personInfo = $"Имя: {Name}, Дата рождения: {Birthday}";
+            if (relationship != null)
+            {
+                personInfo += $" {relationship}";
+            }
+            Console.WriteLine($"{prefix} {personInfo}");
+
+            foreach (var child in Children)
+            {
+                string childRelationship = child.Sex == Gender.Female ? " Дочь" : " Сын";
+                child.PrintFamilyTree(level + 1, childRelationship);
+            }
+
+            foreach (var sibling in Siblings)
+            {
+                string siblingRelationship = sibling.Sex == Gender.Female ? " Сестра" : " Брат";
+                sibling.PrintFamilyTree(level, siblingRelationship);
+            }
+        }
 
 
     }
